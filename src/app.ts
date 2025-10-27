ce36be3f231941fa32d52cab3a62755b82273fa5
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-10-27 18:11:13 +0600

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
-import express, { type Application } from "express";
+import express, { Request, Response, type Application } from "express";
+import cors from "cors";
+import { envConfig } from "./app/config/envConfig";
+import { uptime } from "process";
+import { timeStamp } from "console";
+import globalErrorhandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app: Application = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    success: true,
+    message: "server is running...",
+    environment: envConfig.NODE_ENV,
+    uptime: process.uptime().toFixed(2) + " sec",
+    timeStamp: new Date().toISOString(),
+  });
+});
+
+app.use(globalErrorhandler);
+app.use(notFound);
 
 export default app;
