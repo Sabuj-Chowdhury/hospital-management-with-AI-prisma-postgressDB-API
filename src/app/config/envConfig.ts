35ff6876b7845ad65b1671363d0686aa1dfd7ffc
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-10-27 19:01:26 +0600

    Setting up Prisma with migration

--- src/app/config/envConfig.ts
+++ src/app/config/envConfig.ts
@@ -5,11 +5,12 @@ dotenv.config();
 interface IEnv {
   PORT: string;
   NODE_ENV: "development" | "production";
+  DATABASE_URL: string;
 }
 
 // load env variables
 const envVariables = (): IEnv => {
-  const requiredVariables: string[] = ["PORT", "NODE_ENV"];
+  const requiredVariables: string[] = ["PORT", "NODE_ENV", "DATABASE_URL"];
 
   requiredVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -20,6 +21,7 @@ const envVariables = (): IEnv => {
   return {
     PORT: process.env.PORT as string,
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
+    DATABASE_URL: process.env.DATABASE_URL as string,
   };
 };
 
