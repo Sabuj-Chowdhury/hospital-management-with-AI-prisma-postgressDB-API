dcc04be1e1c8f10a078b06058b017cac23b29458
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-10-28 07:27:32 +0600

    create patient service for create a user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
 import { UserService } from "./user.service";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
   // console.log(`Controller create patient `, req.body);
   const payload = req.body;
   const result = await UserService.createPatient(payload);
-  console.log(result);
+  // console.log(result);
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Patient created successfully!",
+    data: result,
+  });
 });
 
 export const UserController = {
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,29 @@
+import { prisma } from "../../config/prismaInstance";
 import { CreatePatientInput } from "./user.interface";
+import bcrypt from "bcryptjs";
 
-const createPatient = async (payload: CreatePatientInput) => {};
+const createPatient = async (payload: CreatePatientInput) => {
+  const hashedPassword = await bcrypt.hash(payload.password, 10);
+
+  // transaction rollback
+  const result = await prisma.$transaction(async (tnx) => {
+    await tnx.user.create({
+      data: {
+        email: payload.email,
+        password: hashedPassword,
+      },
+    });
+
+    return await tnx.patient.create({
+      data: {
+        email: payload.email,
+        name: payload.name,
+      },
+    });
+  });
+
+  return result;
+};
 
 export const UserService = {
   createPatient,
