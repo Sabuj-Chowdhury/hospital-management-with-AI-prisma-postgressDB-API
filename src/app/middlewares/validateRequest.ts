4b491df893672d7a9eb35d6d58b0846ecc7346c7
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-10-30 18:42:10 +0600

    Some changes in validate request

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -5,10 +5,12 @@ export const validateRequest =
   (zodSchema: ZodObject) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await zodSchema.parseAsync({
-        body: req.body,
-      });
-      return next();
+      if (req.body.data) {
+        req.body = JSON.parse(req.body.data);
+      }
+      req.body = await zodSchema.parseAsync(req.body);
+
+      next();
     } catch (error) {
       next(error);
     }
