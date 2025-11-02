7712a56b2cf9a6e279553110703751e92deda088
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-11-02 09:32:15 +0600

    create doctor zod schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,3 +1,4 @@
+import { Gender } from "@prisma/client";
 import z from "zod";
 
 export const createPatientZodSchema = z.object({
@@ -15,5 +16,24 @@ export const createAdminZodSchema = z.object({
     name: z.string({ error: "name is required" }),
     email: z.email(),
     contactNumber: z.string({ error: "Contact number is required" }),
+  }),
+});
+
+export const createDoctorZodSchema = z.object({
+  password: z.string({ error: "Password required" }),
+  doctor: z.object({
+    name: z.string({ error: "name is required" }),
+    email: z.email(),
+    contactNumber: z.string({ error: "Contact number is required" }),
+    address: z.string().optional(),
+    registrationNumber: z.string({ error: "Registration number is required!" }),
+    experience: z.number().optional(),
+    gender: z.enum([Gender.MALE, Gender.FEMALE]),
+    appointmentFee: z.number({ error: "Appointment fee is required!" }),
+    qualification: z.string({ error: "Qualification is required!" }),
+    currentWorkingPlace: z.string({
+      error: "Current working place is required!",
+    }),
+    designation: z.string({ error: "Designation is required!" }),
   }),
 });
